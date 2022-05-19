1fa3732b64c1430baf52a5548431f5818fea13df
Maksim Chulkov <maksim.chulkov.95@gmail.com>
2022-05-19 12:23:45 +0500

    dev

--- src/Main.java
+++ src/Main.java
@@ -13,25 +13,58 @@ public class Main
         FolderSizeCalculator calculator = new FolderSizeCalculator(file);
         ForkJoinPool pool = new ForkJoinPool();
         long size = pool.invoke(calculator);
-        System.out.println(size);
+        System.out.println(getHumanReadableSize(size));
+        System.out.println(getSizeFromHumanReadable(getHumanReadableSize(size)));
 
         long duration = System.currentTimeMillis() - start;
         System.out.println(duration/1000 + "seconds");
 
     }
 
-    public static long getFolderSize(File folder)
+//    public static long getFolderSize(File folder)
+//    {
+//        if (folder.isFile()) {
+//            return folder.length();
+//        }
+//
+//        long sum = 0;
+//        File[] files = folder.listFiles();
+//        for (File file : files)
+//        {
+//            sum += getFolderSize(file);
+//        }
+//        return sum;
+//    }
+
+    public static String getHumanReadableSize(long size)
     {
-        if (folder.isFile()) {
-            return folder.length();
+        int count = 0;
+        while (size >= 1024) {
+            size /= 1024;
+            count++;
         }
 
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for (File file : files)
-        {
-            sum += getFolderSize(file);
-        }
-        return sum;
+        String readableSize = String.valueOf(size);
+
+        return switch (count) {
+            case 1 -> readableSize + "K";
+            case 2 -> readableSize + "M";
+            case 3 -> readableSize + "G";
+            case 4 -> readableSize + "T";
+            default -> readableSize + "b";
+        };
+    }
+
+    public static long getSizeFromHumanReadable(String size)
+    {
+        long sizeFromHumanReadable = Long.parseLong(size.substring(0, size.length() - 1));
+        char c = size.charAt(size.length() - 1);
+        return switch (c) {
+            case 'K' -> sizeFromHumanReadable * 1024;
+            case 'M' -> sizeFromHumanReadable * 1024 * 1024;
+            case 'G' -> sizeFromHumanReadable * 1024 * 1024 * 1024;
+            case 'T' -> sizeFromHumanReadable * 1024 * 1024 * 1024 * 1024;
+            default -> sizeFromHumanReadable;
+        };
     }
 }
