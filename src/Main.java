8b4893e33fd88e6bace46e5a15ee3ee60bb01b7f
Maksim Chulkov <maksim.chulkov.95@gmail.com>
2022-05-19 10:43:42 +0500

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.io.File;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        String folderPath = "E:/Games/PREY";
+        File file = new File(folderPath);
 
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder)
+    {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files)
+        {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
