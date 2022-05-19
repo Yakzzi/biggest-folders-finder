d5a0d40ac77f19b6df404031f7654b03f5319af6
Maksim Chulkov <maksim.chulkov.95@gmail.com>
2022-05-19 11:15:45 +0500

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        String folderPath = "E:/Games/PREY";
+        String folderPath = "E:/Games";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration/1000 + "seconds");
+
     }
 
     public static long getFolderSize(File folder)
